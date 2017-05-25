6d829acb7a1af2de9c17aec1a793595d05c48900
UndefinedOffset <echipman@webbuildersgroup.com>
2017-05-25 16:45:35 -0300

    Fixed issue where after saving the browser would not be redirected away from the form handler

--- code/forms/FrontEndGridFieldDetailForm.php
+++ code/forms/FrontEndGridFieldDetailForm.php
@@ -190,6 +190,66 @@ class FrontEndGridFieldDetailForm_ItemRequest extends GridFieldDetailForm_ItemRe
         return;
     }
     
+    public function doSave($data, $form) {
+        $new_record=$this->record->ID == 0;
+        $controller=$this->getToplevelController();
+        $list=$this->gridField->getList();
+
+        if(!$this->record->canEdit()) {
+            return $controller->httpError(403);
+        }
+
+        if(isset($data['ClassName']) && $data['ClassName']!=$this->record->ClassName) {
+            $newClassName=$data['ClassName'];
+            // The records originally saved attribute was overwritten by $form->saveInto($record) before.
+            // This is necessary for newClassInstance() to work as expected, and trigger change detection
+            // on the ClassName attribute
+            $this->record->setClassName($this->record->ClassName);
+            // Replace $record with a new instance
+            $this->record=$this->record->newClassInstance($newClassName);
+        }
+
+        try {
+            $form->saveInto($this->record);
+            $this->record->write();
+            $extraData=$this->getExtraSavedData($this->record, $list);
+            $list->add($this->record, $extraData);
+        }catch(ValidationException $e) {
+            $form->sessionMessage($e->getResult()->message(), 'bad', false);
+            
+            Session::set("FormInfo.{$form->FormName()}.errors", array());
+            Session::set("FormInfo.{$form->FormName()}.data", $form->getData());
+            
+            return $controller->redirectBack();
+        }
+
+        // TODO Save this item into the given relationship
+
+        $link='<a href="'.$this->Link('edit').'">"'.htmlspecialchars($this->record->Title, ENT_QUOTES).'"</a>';
+        $message=_t(
+                    'GridFieldDetailForm.Saved',
+                    'Saved {name} {link}',
+                    array(
+                        'name'=>$this->record->i18n_singular_name(),
+                        'link'=>$link
+                    )
+                );
+
+        $form->sessionMessage($message, 'good', false);
+
+        if($new_record) {
+            return $controller->redirect($this->Link());
+        }else if($this->gridField->getList()->byId($this->record->ID)) {
+            return $controller->redirectBack();
+        }else {
+            // Changes to the record properties might've excluded the record from
+            // a filtered list, so return back to the main view if it can't be found
+            $noActionURL=$controller->removeAction($data['url']);
+            $controller->getRequest()->addHeader('X-Pjax', 'Content');
+            return $controller->redirect($noActionURL, 302);
+        }
+    }
+    
     public function doDelete($data, $form) {
         $title=$this->record->Title;
         try {
