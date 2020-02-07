8a7487bb22520b6c8c79e18618c77ebf3e06310d
UndefinedOffset <echipman@webbuildersgroup.com>
2020-02-07 15:22:36 -0400

    GridFieldConfig_Base no longer adds the detail form component

--- src/Forms/GridField/GridFieldConfig_Base.php
+++ src/Forms/GridField/GridFieldConfig_Base.php
@@ -2,7 +2,6 @@
 namespace WebbuildersGroup\FrontEndGridField\Forms\GridField;
 
 use SilverStripe\Forms\GridField\GridFieldConfig_Base as SS_GridFieldConfig_Base;
-use SilverStripe\Forms\GridField\GridFieldDetailForm as SS_GridFieldDetailForm;
 use SilverStripe\Forms\GridField\GridFieldFilterHeader;
 use SilverStripe\Forms\GridField\GridField_ActionMenu;
 
@@ -14,10 +13,6 @@ class GridFieldConfig_Base extends SS_GridFieldConfig_Base
     public function __construct($itemsPerPage = null)
     {
         parent::__construct($itemsPerPage);
-        
-        $this
-            ->removeComponentsByType(SS_GridFieldDetailForm::class)
-            ->addComponent(new GridFieldDetailForm());
         
         if (!GridField::config()->use_admin_api) {
             $this->removeComponentsByType(GridField_ActionMenu::class);
