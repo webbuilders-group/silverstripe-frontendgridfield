ab8e96c2c4fe349abf2c9d361017de4bcf8433ea
UndefinedOffset <echipman@webbuildersgroup.com>
2021-04-14 17:03:41 -0300

    Fixed issue where dialogs would not work because of a missing instance of ssui.core.js

--- src/Forms/GridField/GridField.php
+++ src/Forms/GridField/GridField.php
@@ -12,9 +12,9 @@ class GridField extends SS_GridField
      * @var bool
      */
     private static $use_admin_api = false;
-    
+
     protected $useAdminAPI = false;
-    
+
     /**
      * Returns the whole gridfield rendered with all the attached components
      * @return string
@@ -24,8 +24,8 @@ class GridField extends SS_GridField
         Requirements::block('silverstripe/admin: css/GridField.css');
         Requirements::css('webbuilders-group/silverstripe-frontendgridfield: css/FrontEndGridField.css');
         Requirements::themedCSS('FrontEndGridField');
-        
-        
+
+
         if ($this->getUseAdminAPI()) {
             Requirements::javascriptTemplate(dirname(__FILE__) . '/../../../javascript/boot.template.js', [
                 'SecurityID' => Convert::raw2js(SecurityToken::inst()->getValue()),
@@ -34,7 +34,7 @@ class GridField extends SS_GridField
                 'Environment' => Convert::raw2js(Director::get_environment_type()),
                 'Debugging' => (Director::isDev() ? 'true' : 'false')
             ]);
-            
+
             Requirements::javascript('silverstripe/admin: client/dist/js/vendor.js');
             Requirements::javascript('silverstripe/admin: client/dist/js/bundle.js');
             Requirements::add_i18n_javascript('silverstripe/admin: javascript/lang');
@@ -45,15 +45,16 @@ class GridField extends SS_GridField
             Requirements::javascript('silverstripe/admin: client/dist/js/i18n.js');
             Requirements::add_i18n_javascript('silverstripe/admin: javascript/lang');
             Requirements::javascript('webbuilders-group/silverstripe-frontendgridfield: javascript/externals/silverstripe/lib.js');
+            Requirements::javascript('webbuilders-group/silverstripe-frontendgridfield: javascript/externals/silverstripe/ssui.core.js');
             Requirements::javascript('webbuilders-group/silverstripe-frontendgridfield: javascript/GridField.js');
         }
-        
+
         Requirements::javascript('webbuilders-group/silverstripe-frontendgridfield: javascript/FrontEndGridField.js');
-        
-        
+
+
         return parent::FieldHolder();
     }
-    
+
     /**
      * Allow this GridField to use the Admin's API, you should not mix and match this otherwise you will run into issues!
      * @param bool $value Whether or not to use the admin's api
@@ -64,7 +65,7 @@ class GridField extends SS_GridField
         $this->useAdminAPI = $value;
         return $this;
     }
-    
+
     /**
      * Get's whether this GridField is to use the Admin's API
      * @return Whether or not to use the admin's api
@@ -73,7 +74,7 @@ class GridField extends SS_GridField
     {
         return $this->useAdminAPI || $this->config()->use_admin_api;
     }
-    
+
     /**
      * Gets the type for this field
      * @return string
@@ -82,7 +83,7 @@ class GridField extends SS_GridField
     {
         return 'frontendgrid ' . parent::Type();
     }
-    
+
     /**
      * Custom Readonly transformation to remove actions which shouldn't be present for a readonly state.
      * @return GridField
@@ -90,7 +91,7 @@ class GridField extends SS_GridField
     public function performReadonlyTransformation()
     {
         $this->readonlyComponents[] = GridFieldDetailForm::class;
-        
+
         return parent::performReadonlyTransformation();
     }
 }
